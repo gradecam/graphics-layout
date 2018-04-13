5b2f6a5d43ee443ce6fd962102dc1322a35f8246
Rick Gigger <rgigger@clstalent.com>
2018-04-12 18:31:41 -0600

    expose htmlToViewDescTree

--- src/index.ts
+++ src/index.ts
@@ -16,6 +16,6 @@ export {
 export {View} from './View';
 export {HorizontalAlignment, VerticalAlignment} from './Alignment';
 export {FrameView} from './FrameView';
-export {renderHtml, HtmlRenderOpts, FontSelector, CssSelectorMap} from './HtmlRenderer';
+export {renderHtml, htmlToViewDescTree, HtmlRenderOpts, FontSelector, CssSelectorMap} from './HtmlRenderer';
 export {Point} from './Context';
 export { PDFDocument };
